c9189779dd90f7219114e17f1ab33a9dda467ce3
GregDavidson <greg.davidson+git@gmail.com>
2023-04-25 22:41:53 -0700

    fixed indentation

--- FizzBuzz/fizzbuzz.scm
+++ FizzBuzz/fizzbuzz.scm
@@ -4,14 +4,14 @@
   (define (bump n init) (if (= 0 n) init (- n 1)))
   (define (iter i fizz buzz)
     (if (<= i i-max)
-	(begin
-	  (if (= 0 fizz) (display 'fizz))
-	  (if (= 0 buzz) (display 'buzz))
-	  (if (and (> fizz 0) (> buzz 0)) (display i))
-	  (newline)
-	  (iter (+ i 1)
-		(bump fizz fizz-init)
-		(bump buzz buzz-init)) ) ) )
+        (begin
+          (if (= 0 fizz) (display 'fizz))
+          (if (= 0 buzz) (display 'buzz))
+          (if (and (> fizz 0) (> buzz 0)) (display i))
+          (newline)
+          (iter (+ i 1)
+                (bump fizz fizz-init)
+                (bump buzz buzz-init)) ) ) )
   (iter 1 fizz-init buzz-init) )
 (fizzbuzz 100 2 4)
 
